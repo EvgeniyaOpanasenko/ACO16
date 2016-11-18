ef06f27b6886b9ef24f018baf635bbf496ebd5d6
Evgeniya <evgeniya.opanasenko@outlook.com>
2016-11-18 12:31:03 +0200

    add List interface

--- src/main/java/demo/ExceptionDemo/MyExceptionDemo.java
+++ src/main/java/demo/ExceptionDemo/MyExceptionDemo.java
@@ -3,18 +3,12 @@ package demo.ExceptionDemo;
 /**
  * Created by Lisa on 11/12/2016.
  */
-public class MyExceptionDemo {
+public class MyExceptionDemo extends Throwable {
+    public static void main(String[] args) throws MyExceptionDemo {
+      String str = new String("Hello Dolly");
+        if (str.contains("o"));
+    }
 
-    String word = "I love my somthing";
-    /*try
-    {
-        if (word.contains("o")){
-            try {
-                throw new MyException();
-            } catch (MyException e) {
-                e.printStackTrace();
-            }
-        }
-    }*/
+
 }
 
--- src/main/java/demo/ZeroException.java
+++
@@ -1,8 +0,0 @@
-package demo;
-
-/**
- * Created by Lisa on 11/12/2016.
- */
-public class ZeroException extends Exception {
-
-}
